ab93898f85967fdb1fc3f58c8ee90b8cf9634ee3
sladkiy_bubalex <zaytsev.dmitry9228@gmail.com>
2025-05-29 17:34:38 +0300

    update + fix expensia service db creds

--- data/postgres/init.sql
+++ data/postgres/init.sql
@@ -1,6 +1,6 @@
-create database user_service_db;
-CREATE USER user_service_user WITH LOGIN PASSWORD '8c6b18ac-84ff-4436-8916-6e11aaa41e92';
-GRANT ALL PRIVILEGES ON DATABASE user_service_db TO user_service_user;
+create database user_service;
+CREATE USER user_service WITH LOGIN PASSWORD '8c6b18ac-84ff-4436-8916-6e11aaa41e92';
+GRANT ALL PRIVILEGES ON DATABASE user_service TO user_service;
 
 create database notes;
 CREATE USER notes_user WITH LOGIN PASSWORD '5fea3e86-29b6-4f85-a5ed-3117d7ff7c7f';
@@ -10,10 +10,10 @@ create database keycloak;
 CREATE USER keycloak_user WITH LOGIN PASSWORD '32f21777-78c6-49bb-a5fe-b3ebeb325593';
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak_user;
 
-create database telegram_bot;
-CREATE USER telegram_bot_user WITH LOGIN PASSWORD '9876c88e-94a6-45c0-8e88-161d7519bf12';
-GRANT ALL PRIVILEGES ON DATABASE telegram_bot TO telegram_bot_user;
+create database bot_service;
+CREATE USER bot_service_user WITH LOGIN PASSWORD '9876c88e-94a6-45c0-8e88-161d7519bf12';
+GRANT ALL PRIVILEGES ON DATABASE bot_service TO bot_service_user;
 
-create database expensia_db;
+create database expensia_service;
 CREATE USER expensia_user WITH LOGIN PASSWORD '4a3cac82-eeb2-4b5e-b05f-8c3a5d608c82';
-GRANT ALL PRIVILEGES ON DATABASE expensia_db TO expensia_user;
+GRANT ALL PRIVILEGES ON DATABASE expensia_service TO expensia_user;
